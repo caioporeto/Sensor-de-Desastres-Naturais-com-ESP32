278879eabe95f874a59d4d89ae67a0bb7840f02e
Luan Guio Cotini <luanpgc9@gmail.com>
2025-05-04 17:29:16 -0300

    acrescentei ".ino" no arquivo e comentei algumas linhas

--- Sensor-de-Desastres-Naturais-com-ESP32/hardware/portadora_e_moduladora.ino
+++ Sensor-de-Desastres-Naturais-com-ESP32/hardware/portadora_e_moduladora.ino
@@ -1,4 +1,4 @@
-#define OUTPUT_PIN1 17
+#define OUTPUT_PIN1 17 
 #define OUTPUT_PIN2 16
 #define FREQ 125000
 #define RESOLUTION 8
@@ -7,12 +7,13 @@
 que vai ser transformada em uma onda senoidal de 125kHz 
 na saída do filtro */
 void setup() {
+  // Inicializa a serial a 115200 bps
   Serial.begin(115200);
-  bool sucess = ledcAttach(OUTPUT_PIN1, FREQ, RESOLUTION);
+  bool sucess = ledcAttach(OUTPUT_PIN1, FREQ, RESOLUTION); // Usa PWM para gerar uma onda quadrada de exatamente 125kHz
   if (!sucess) {
     Serial.println(" Falha ao configurar o PWM ");
   }
-  ledcWrite(OUTPUT_PIN1, 128); // duty cicle de 50% (128 é metade de 256 [2^8])
+  ledcWrite(OUTPUT_PIN1, 128); // Duty cicle de 50% (128 é metade de 256 [2^8])
   Serial.println(" Onda quadrada de 125 kHz gerada! ");
   pinMode(16, OUTPUT);
 }
